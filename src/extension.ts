be1f050dcdc4361e1938bc11aee30db6414ee7d4
trjmig <jgomes@gmv.com>
2023-07-18 16:00:20 +0100

    Version 0.00

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,37 @@ import axios from 'axios';
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('code-summarizer',  async () => {
+  let doxigiaCommand = vscode.commands.registerCommand('extension.doxigia', () => {
+ 
+    let commandMappings = [
+      {
+        commandId: 'code-summarizer-doxygen',
+        handler: doxygenComments
+      },
+      {
+        commandId: 'inLineComments-comments',
+        handler: inLineComments
+      },
+      {
+        commandId: 'code-headers',
+        handler: putHeaders
+      },
+      {
+        commandId: 'code-explanation',
+        handler: explainCode
+      }
+    ];
+    
+    let disposables = commandMappings.map(({ commandId, handler }) =>
+      vscode.commands.registerCommand(commandId, handler)
+    );
+    
+    disposables.forEach(disposable => context.subscriptions.push(disposable));  
+  });
+  context.subscriptions.push(doxigiaCommand);
+}
+
+async function doxygenComments() {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return; // No open text editor
@@ -18,36 +48,154 @@ export async function activate(context: vscode.ExtensionContext) {
 
 		// Get selected code and strip out whitespace and lower case all tokens
 		let code = document.getText(selection);
-    	//console.log(code);
 
-		// Prompt user for comment using ChatGPT API
-		const comment = await promptComment(code);
-		//console.log(comment);
-    	context.subscriptions.push(disposable);
-  });
-}
+    const prompt = `Give comments for the code I will provide in a doxygen way. 
+    But keep in mind this aspects:
+    With all the parameters of the doxygen format, 
+    the @brief followed by the explanation of the code, @param followed by the input parameters and their
+    explanation in that part of the code, the @return and say if or what it returns. Also if you see fit add commonly 
+    used tags include @note, @warning, @see, @example, etc. Use these tags to convey important information, warnings, 
+    references to related documentation, or code examples. 
+    Don't forget to keep comments concise and focused: Write comments that are clear, concise, and focused on the 
+    purpose and functionality of the code element. Avoid unnecessary verbosity or repetition. Maintain consistent 
+    formatting: Use consistent formatting and indentation throughout the comments to enhance readability. 
+    Remember that good documentation should be informative, clear, and useful to both developers and users of your code. 
+    
+    The code is the following: ${code}\n`;
 
-async function promptComment(code: string): Promise<string> {
-    const response = await chatGptApiCall(code);
-    const comment = response;
-
-	const editor = vscode.window.activeTextEditor;
-    if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
-        const startPosition = new vscode.Position(selection.start.line, 0);
-        const commentText = `${comment}\n\n`;
-        await editor.edit(editBuilder => {
-            editBuilder.insert(startPosition, commentText);
-        });
-    }
-    return comment;
-}
-
-async function chatGptApiCall(code: string): Promise<string> {
-    const prompt = `/**\n * @brief Comment for the code in a doxygen way with @brief, @param, @return:\n * ${code}\n */`;
     const response = await openaiApiCall(prompt);
-    return response;
+		// Prompt user for comment using ChatGPT API
+		await promptComment(response);
+}
+
+async function inLineComments() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+
+  const document = editor.document;
+  const selection = editor.selection;
+
+  // Get selected code and strip out whitespace and lower case all tokens
+  let code = document.getText(selection);
+
+  const prompt = `I want you to explain the code I will provide the best way you can with just InLine comments.
+  But keep in mind this aspects:
+  The comments can be put on top of the line you are explaining.
+  Don't forget to keep comments concise and focused: Write comments that are clear, concise, and focused on the 
+  purpose and functionality of the code element. Avoid unnecessary verbosity or repetition. Maintain consistent.
+  Remember that good documentation should be informative, clear, and useful to both developers and users of your code. 
+    
+  The code is the following:   ${code}\n`;
+
+  const response = await openaiApiCall(prompt);
+  // Prompt user for comment using ChatGPT API
+  await promptComment(response);
+}
+
+async function putHeaders() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+
+  const document = editor.document;
+  const selection = editor.selection;
+
+  // Get selected code and strip out whitespace and lower case all tokens
+  let code = document.getText(selection);
+
+  const prompt = `I want you to give me a header for this all code that is in the same file. 
+  
+  The header needs to be in a format similar to this one:
+  
+  
+  /* ============================================================================
+ *  Copyright (C) GMVIS Skysoft S.A., 2018
+ * ============================================================================
+ *  This file is part of the AIR - ARINC 653 Interface in RTEMS - Operating
+ *  system.
+ *  The license and distribution terms for this file may be found in the file
+ *  LICENSE in this distribution or at http://www.rtems.com/license/LICENSE.
+ * ==========================================================================*/
+/**
+ * @file added my author
+ * @author added my author
+ * @brief Here I want you to give a brief about all the selected code but keep it concise and focused.
+ * Write comments that are clear, concise, and focused on the purpose and functionality of the code element.
+ * Avoid unnecessary verbosity or repetition. Maintain consistent. Remember that good documentation should 
+ * be informative, clear, and useful to both developers and users of your code. 
+ */
+
+  The code is the following: ${code}\n`;
+
+  const response = await openaiApiCall(prompt);
+  // Prompt user for comment using ChatGPT API
+  if (editor) {
+      const document = editor.document;
+      const selection = editor.selection;
+
+      const startPosition = new vscode.Position(selection.start.line, 0);
+      const rangeToReplace = new vscode.Range(selection.start, selection.end);
+
+      await editor.edit(editBuilder => {
+        editBuilder.insert(startPosition,response);
+        editBuilder.delete(rangeToReplace);
+        //editBuilder.replace(rangeToReplace, response);
+      });
+  }
+}
+
+async function explainCode() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+
+  const document = editor.document;
+  const selection = editor.selection;
+
+  // Get selected code and strip out whitespace and lower case all tokens
+  let code = document.getText(selection);
+
+  const prompt = `I want you explain this portion of the code in the way possible. 
+  Comment it and put the comments on top of the code I gave you and inside /** */
+
+  Basically I want concise and focused comments. Write comments that are clear, concise, and focused 
+  on the purpose and functionality of the code element. Avoid unnecessary verbosity or repetition. Maintain
+  consistent. Remember that good documentation should be informative, clear, and useful to both developers and
+  users of your code. 
+
+  So the comments should be
+
+  /** 
+   * comments
+   * /
+  code
+  
+  The code is the following: ${code}\n`;
+
+  const response = await openaiApiCall(prompt);
+  // Prompt user for comment using ChatGPT API
+  await promptComment(response);
+}
+
+async function promptComment(response: string ){
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const document = editor.document;
+      const selection = editor.selection;
+
+      const startPosition = new vscode.Position(selection.start.line, 0);
+      const rangeToReplace = new vscode.Range(selection.start, selection.end);
+
+      await editor.edit(editBuilder => {
+        editBuilder.insert(startPosition,response);
+        editBuilder.delete(rangeToReplace);
+        //editBuilder.replace(rangeToReplace, response);
+      });
+    }
 }
 
 async function openaiApiCall(prompt: string): Promise<string>{
@@ -71,7 +219,6 @@ async function openaiApiCall(prompt: string): Promise<string>{
 
     const response = await axios.post(url, data, { headers });
     const comment = response.data.choices[0].message.content.trim();
-	//console.log(comment);
     return comment;
   } catch (error) {
     console.error('Error calling OpenAI API:', error);
